78fe60835dc51d9715c6987760ffff0ab21e67a8
joel28cachii <joel28cachii@gmail.com>
2013-08-09 17:00:57 +0000

    Se añadio funciones para controlar los programas y sus categorias asi como controlar las provincias, distritos y corregimientos de nacimiento.

--- funciones.js
+++ funciones.js
@@ -3,7 +3,9 @@ $(document).ready(inicio)
 
 function inicio(){
 	$("#provincias").change(cargarDistritos);
+	$("#provinciasnacimiento").change(cargarDistritosnacimiento);
 	$("#distritos").change(cargarCorregimientos);
+	$("#distritosnacimiento").change(cargarCorregimientosnacimiento);
 	$("#programas").change(cargarCategorias)
 }
 function cargarDistritos(){
@@ -13,10 +15,24 @@ function cargarDistritos(){
 	});	
 	return false;
 } 
+function cargarDistritosnacimiento(){
+	var p = $("#provinciasnacimiento").attr("value");
+	$.post("./mvc/vista/domiciliaria_capturardatos_distritosnacimiento.php",{idprovincia:p},function(resultado){
+		$("#mostrardistritosnacimiento").html(resultado)
+	});	
+	return false;
+} 
 function cargarCorregimientos(){
 	var d = $("#distritos").attr("value");
 	$.post("./mvc/vista/domiciliaria_capturardatos_corregimientos.php",{iddistrito:d},function(resultado){
 		$("#mostrarcorregimientos").html(resultado)
+	});
+	return false;
+}
+function cargarCorregimientosnacimiento(){
+	var d = $("#distritosnacimiento").attr("value");
+	$.post("./mvc/vista/domiciliaria_capturardatos_corregimientosnacimiento.php",{iddistrito:d},function(resultado){
+		$("#mostrarcorregimientosnacimiento").html(resultado)
 	});
 	return false;
 }
