715031507c44452445c5b00dce4d601789304129
luis2531@gmail.com <luis2531@gmail.com@1dcdc633-3ce7-5c84-3643-f052b9647ef9>
2013-08-09 17:00:53 +0000

    Función de Jquery para buscar paciente

---
+++ trunk/funcionbuscar.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+                                
+        var consulta;
+                                                                          
+         //hacemos focus al campo de búsqueda
+        $("#buscar").focus();
+                                                                                                    
+        //comprobamos si se pulsa una tecla
+        $("#buscar").keyup(function(e){
+                                     
+              //obtenemos el texto introducido en el campo de búsqueda
+              consulta = $("#buscar").val();
+                                                                           
+              //hace la búsqueda
+                                                                                  
+              $.ajax({
+                    type: "POST",
+                    url: "./mvc/vista/buscardistrito.php",
+                    data: "b="+consulta,
+                    dataType: "html",
+                   /* beforeSend: function(){
+                          //imagen de carga
+                          $("#resultado").html("<p align='center'><img src='ajax-loader.gif' /></p>");
+                    },*/
+                    error: function(){
+                          alert("error petición ajax");
+                    },
+                    success: function(data){                                                    
+                          $("#resultado").empty();
+                          $("#resultado").append(data);
+                    }
+              });                                                           
+        });                                                     
+});
