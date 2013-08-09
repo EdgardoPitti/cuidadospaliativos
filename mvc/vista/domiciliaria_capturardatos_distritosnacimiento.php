4617eee8a713b2755cdba35c14a8dcb22c4b1949
joel28cachii <joel28cachii@gmail.com>
2013-08-09 17:01:47 +0000

    Pagina para controlar los distritos de nacimiento

---
+++ mvc/vista/domiciliaria_capturardatos_distritosnacimiento.php
@@ -0,0 +1,18 @@
+<?php
+	include_once('../modelo/Accesatabla.php');
+	$distritos = new Accesatabla('distritos');
+	include_once('../modelo/diseno.php');
+	$pg = new Diseno();
+	$cont = '<select id="distritosnacimiento" name="distritosnacimiento"  style="width:140px">
+				<option value="0"></option>
+	';
+	$d = $distritos->buscardonde('id_provincia = '.$idprovincia.'');
+	while($d){
+		$cont.='<option value="'.$distritos->obtener('id').'">'.$distritos->obtener('descripcion').'</option>';
+		$d = $distritos->releer();
+	}
+	$cont.='</select>';
+	echo $pg->latino($cont);
+?>
+<script src="jquery.js"></script>
+<script src="funciones.js"></script>
